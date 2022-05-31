ece9cac6ccfe73bd8ddc727115e0373025612a26
liulei <278603540@qq.com>
2022-05-31 09:16:20 +0800

    spring boot email 提交

---
+++ spring-boot-actuator/src/test/java/com/learning/actuator/SpringBootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-boot-email/src/main/java/com/learning/email/controller/EmailController.java
@@ -0,0 +1,109 @@
+package com.learning.email.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private JavaMailSender jms;
+    
+    @Value("${spring.mail.username}")
+    private String from;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+    
+    @RequestMapping("sendSimpleEmail")
+    public String sendSimpleEmail() {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo("278603540@qq.com"); // 接收地址
+            message.setSubject("一封简单的邮件"); // 标题
+            message.setText("使用Spring Boot发送简单邮件。"); // 内容
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendHtmlEmail")
+    public String sendHtmlEmail() {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("278603540@qq.com"); // 接收地址
+            helper.setSubject("一封HTML格式的邮件"); // 标题
+            // 带HTML格式的内容
+            StringBuffer sb = new StringBuffer("<p style='color:#6db33f'>使用Spring Boot发送HTML格式邮件。</p>");
+            helper.setText(sb.toString(), true);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendAttachmentsMail")
+    public String sendAttachmentsMail() {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("278603540@qq.com"); // 接收地址
+            helper.setSubject("一封带附件的邮件"); // 标题
+            helper.setText("详情参见附件内容！"); // 内容
+            // 传入附件
+            FileSystemResource file = new FileSystemResource(new File("C:/Users/adc-106/Desktop/项目文档.docx"));
+            helper.addAttachment("项目文档.docx", file);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("sendTemplateEmail/{code}")
+    public String sendTemplateEmail(@PathVariable("code") String code) {
+        MimeMessage message = null;
+        try {
+            message = jms.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("278603540@qq.com"); // 接收地址
+            helper.setSubject("邮件摸板测试"); // 标题
+            // 处理邮件模板
+            Context context = new Context();
+            context.setVariable("code", code);
+            String template = templateEngine.process("emailTemplate", context);
+            helper.setText(template, true);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
