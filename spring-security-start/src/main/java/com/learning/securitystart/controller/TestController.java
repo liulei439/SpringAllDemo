a5f550575c1902e4368c6c7b75c1858e3388a1e7
liulei <278603540@qq.com>
2022-06-01 16:42:27 +0800

    spring boot springSecurityStart 提交

---
+++ spring-security-start/src/main/java/com/learning/securitystart/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.learning.securitystart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
---
+++ spring-security-start/src/test/java/com/learning/securitystart/SpringSecurityStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.securitystart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
